0d7e81c63e2eb14fe8619ab2279eb3efa539f38c
prane12 <pranesh9096@gmail.com>
2023-09-11 23:07:42 +0530

    spring and docker first time

---
+++ dockerspringboot/src/test/java/com/docspringboot/dockerspringboot/DockerspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docspringboot.dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
